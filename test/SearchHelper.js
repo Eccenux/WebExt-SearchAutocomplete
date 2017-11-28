1e270e1133dd5df9f84f81e0a13ea2be05a09153
Eccenux <eccenux+git@gmail.com>
2017-11-28 20:53:42 +0100

    Tests for building URLs.

--- test/SearchHelper.js
+++ test/SearchHelper.js
@@ -38,5 +38,70 @@ describe('SearchHelper', function () {
 			var url = searchHelper.buildSearchUrl(engineExample, action, text);
 			assert.equal(url, 'http://localhost/?abc=' + text);
 		});
+		it('Should work without params', function () {
+			var action = {
+				url : '{baseUrl}',
+				data : {
+				}
+			};
+			var text = 'def';
+			var url = searchHelper.buildSearchUrl(engineExample, action, text);
+			assert.equal(url, 'http://localhost/');
+		});
+		it('Should append a second parameter', function () {
+			var action = {
+				url : '{baseUrl}',
+				data : {
+					abc : 'test',
+					def : 'test2'
+				}
+			};
+			var text = 'nothing';
+			var url = searchHelper.buildSearchUrl(engineExample, action, text);
+			assert.equal(url, 'http://localhost/?abc=test&def=test2');
+		});
+		it('Should escape values', function () {
+			var testValue = 'zażółćgęśląjaźń';
+			var action = {
+				url : '{baseUrl}',
+				data : {
+					testValue : testValue
+				}
+			};
+			var text = 'nothing';
+			var url = searchHelper.buildSearchUrl(engineExample, action, text);
+			url.replace(/testValue=(.+)$/, function(a, transformedValue) {
+				assert.notEqual(transformedValue, testValue, 'Should not match unescaped value');
+				assert.equal(decodeURIComponent(transformedValue), testValue, 'Should decode to original');
+			});
+		});
+		it('Should contain only one question mark', function () {
+			var action = {
+				url : '{baseUrl}',
+				data : {
+					abc : 'te?st',
+					def : 'te?st2',
+					hij : 'te?st3'
+				}
+			};
+			var text = 'nothing';
+			var url = searchHelper.buildSearchUrl(engineExample, action, text);
+			var parts = url.split('?');
+			assert.equal(parts.length - 1, 1);
+		});
+		it('Should separate params correctly', function () {
+			var action = {
+				url : '{baseUrl}',
+				data : {
+					abc : 't&e?st',
+					def : 't&e?st2',
+					hij : 't&e?st3'
+				}
+			};
+			var text = 'nothing';
+			var url = searchHelper.buildSearchUrl(engineExample, action, text);
+			var parts = url.split('&');
+			assert.equal(parts.length - 1, Object.keys(action.data).length - 1);
+		});
 	});
 });
