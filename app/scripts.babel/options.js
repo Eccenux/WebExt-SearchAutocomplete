8f007bbb90e7a5c80a9ba4070b7fc3ca070ea7a9
Eccenux <eccenux+git@gmail.com>
2017-12-29 15:25:21 +0100

    Replace engines list with Angular generated list

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -23,7 +23,13 @@ if (typeof browser != 'undefined') {
 	})
 // in-browser testing examples
 } else {
-	prepareEngines([enWikiEngine, plWikiEngine]);
+	setTimeout(function(){
+		prepareEngines([enWikiEngine, plWikiEngine, {
+			title : 'Just a test',
+			keyword : 't',
+			baseUrl : 'http://test.localhost/'
+		}]);
+	}, 100);
 }
 
 /**
@@ -31,38 +37,20 @@ if (typeof browser != 'undefined') {
  */
 function prepareEngines(engines) {
 	console.log(engines);
-	let container = document.getElementById('engines-container');
+	app.EngineController.engines.length = 0;
 	for (let e = 0; e < engines.length; e++) {
 		let engine = new SearchEngine(engines[e]);
-		
-		let el = document.createElement('li');
-		el.engine = engine;
-		
-		el.textContent = `[${engine.keywords.join(',')}] ${engine.title}`;
-		// edit
-		let button = document.createElement('a');
-		button.addEventListener('click', function(){
-			let engine = this.parentNode.engine;
-			editEngine(engine);
-		});
-		button.textContent = '✏️';
-		el.appendChild(button);
-		// append
-		container.appendChild(el);
+		// add
+		app.EngineController.engines.push(engine);
 	}
+	app.EngineController.$apply();
 }
 
 function editEngine(engine) {
 	console.log(engine);
-	currentEngine.update(engine);
-	app.EngineController.$apply();
+	app.EngineController.currentEngine.update(engine);
+	//app.EngineController.$apply();
 }
-
-window.currentEngine = new SearchEngineModel(new SearchEngine({
-	title : 'Just a test',
-	keyword : 't',
-	baseUrl : 'http://test.localhost/'
-}));
 
 window.app = {};
 angular
@@ -70,6 +58,8 @@ angular
 	.controller('EngineController', function($scope) {
 		app.EngineController = $scope;
 
-		$scope.engine = currentEngine;
+		$scope.currentEngine = new SearchEngineModel();
+		$scope.engines = [];
+		$scope.editEngine = editEngine;
 	})
 ;
