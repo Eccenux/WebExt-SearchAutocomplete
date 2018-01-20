5c6ce8b3f0e140417b42231072881a26da1bf7b1
Eccenux <eccenux+git@gmail.com>
2018-01-21 00:42:53 +0100

    engine models copy

---
+++ app/scripts.babel/inc/SearchCredential.js
@@ -0,0 +1,34 @@
+import SearchEngineAction from './SearchEngineAction.js';
+
+function SearchEngine(engine) {
+	this.keywords = [];
+	if (typeof engine.keyword === 'string') {
+		this.keywords.push(engine.keyword);
+	} else if (typeof engine.keywords === 'string') {
+		if (engine.keywords.search(',')) {
+			let keywords = engine.keywords.replace(/\s+/g, '');
+			this.keywords = keywords.split(',');
+		} else {
+			this.keywords.push(engine.keywords);
+		}
+	} else {
+		this.keywords = [].concat(engine.keywords);
+	}
+
+	this.baseUrl = '';
+	if (typeof engine.baseUrl === 'string') {
+		this.baseUrl = engine.baseUrl;
+	}
+
+	this.title = '';
+	if (typeof engine.title === 'string') {
+		this.title = engine.title;
+	} else {
+		this.title = engine.baseUrl;
+	}
+
+	this.openAction = new SearchEngineAction(engine.openAction || {});
+	this.autocompleteAction = new SearchEngineAction(engine.autocompleteAction || {});
+}
+
+export default SearchEngine;
---
+++ app/scripts.babel/inc/SearchCredentialModel.js
@@ -0,0 +1,80 @@
+/**
+ * Observable search engine model.
+ * 
+ * @param {SearchEngine} engine Optional initial engine.
+ */
+function SearchEngineModel(engine) {
+    this.id = null;
+    this.keywords = '';
+    this.baseUrl = '';
+	this.title = '';
+	this.actions = [];
+	if (engine) {
+		this.update(engine);
+	}
+};
+/**
+ * Update the engine model.
+ * 
+ * @param {SearchEngine} engine 
+ */
+SearchEngineModel.prototype.update = function(engine) {
+	this.id = engine.id;
+    this.keywords = engine.keywords.join(',');
+    this.baseUrl = engine.baseUrl;
+	this.title = engine.title;
+	this.actions.length = 0;
+	this.addAction('open', engine.openAction);
+	this.addAction('autocomplete', engine.autocompleteAction);
+}
+
+/**
+ * Recreate engine definition from the model.
+ */
+SearchEngineModel.prototype.getEngine = function() {
+	let engine = {};
+	engine.id = this.id;
+	engine.keywords = this.keywords;
+	engine.baseUrl = this.baseUrl;
+	engine.title = this.title;
+	for (let a = 0; a < this.actions.length; a++) {
+		const action = this.actions[a];
+		let data = {};
+		for (let d = 0; d < action.data.length; d++) {
+			const dat = action.data[d];
+			data[dat.key] = dat.value;
+		}
+		engine[`${action.name}Action`] = {
+			url: action.url,
+			method: action.method,
+			type: action.type,
+			data: data
+		};
+	}
+	return engine;
+}
+
+/**
+ * Add action to collection.
+ * 
+ * @param {String} name Name of the action to display.
+ * @param {SearchEngineAction} action 
+ */
+SearchEngineModel.prototype.addAction = function(name, action) {
+	let data = [];
+	for (var key in action.data) {
+		data.push({
+			key: key,
+			value: action.data[key]
+		});
+	}
+	this.actions.push({
+		name: name,
+		url: action.url,
+		method: action.method,
+		type: action.type,
+		data: data
+    });
+}
+
+export default SearchEngineModel
