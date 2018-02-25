5e7337359cc91be163b1e3f72a664cb4b93203bb
Eccenux <eccenux+git@gmail.com>
2018-02-26 00:04:28 +0100

    missing i18n helper

--- app/scripts.babel/inc/I18nHelper.js
+++ app/scripts.babel/inc/I18nHelper.js
@@ -1,3 +1,38 @@
+// dummy
+function addMessage() {
+}
+
+// uncomment below to allow in-browser building of missing i18n list
+// format and copy with:
+// copy(JSON.stringify(_i18nMessages.missing, null, '\t'))
+// Note! Messages that are hidden might not show up. Move around the app to get more messages.
+/**
+import messagesPl from '../../_locales/pl/messages';
+import messagesEn from '../../_locales/en/messages';
+//console.log('i18nhelper', messagesPl, messagesEn, wikiTemplateEngine);
+if (typeof window._i18nMessages !== 'object') {
+	window._i18nMessages = {
+		missing: {
+			pl: {},
+			en: {},
+		}
+	};
+}
+function addMessage(messageName, message) {
+	let obj = {
+		'message' : message
+	};
+	if (!(messageName in messagesPl)) {
+		console.log('pl: ', messageName)
+		_i18nMessages.missing.pl[messageName] = obj;
+	}
+	if (!(messageName in messagesEn)) {
+		console.log('en: ', messageName)
+		_i18nMessages.missing.en[messageName] = obj;
+	}
+}
+/**/
+
 /**
  * Get I18n string.
  * 
@@ -5,7 +40,9 @@
  * @sa https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/i18n/getMessage
  */
 let getI18n = (typeof browser != 'undefined') ? browser.i18n.getMessage : function(messageName) {
-	return messageName.replace(/_/g, ' ').replace(/^.+\./, '');
+	let message = messageName.replace(/_/g, ' ').replace(/^.+\./, '');
+	addMessage(messageName, message);
+	return message;
 };
 
 export { getI18n };
